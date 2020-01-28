4ba4e9b11a7fc0e951b27b92cc3caa943f3a2eaf
David Martinez <dmartinez7500@gmail.com>
2020-01-28 16:31:04 -0500

    Testing non-root setup for 2020.1

--- setup_kali.sh
+++ setup_kali.sh
@@ -11,35 +11,31 @@ echo "      1. You've setup VBox Tools/VMWare tools if running in a VM"
 echo "      2. You're running as root, either by login or sudo"
 read -p "Press any key to continue with setup..."
 
-# if [[ $(id -u) != 0 ]]; then
-# 	echo -e "\n[!] Setup script needs to run as root!\n\n"
-# 	exit 0
-# fi
+if [[ $(id -u) != 0 ]]; then
+	echo -e "\n[!] Setup script needs to run as root!\n\n"
+	exit 0
+fi
 
 echo "[*] Please provide the username you want to apply as owner of the tools in /opt..."
 read -p 'Username: ' username
 
 echo -e "\n\n"
 echo "[*] Updating and upgrading system before starting..."
-sudo apt update -y
-sudo apt full-upgrade -y
+apt update -y
+apt full-upgrade -y
 
 echo -e "\n\n"
 echo "[*] Installing pre-reqs..."
-sudo apt install -y git ansible python-pip python3-pip golang-go python-dnspython libpcap-dev libssl-dev libnetfilter-queue-dev
+apt install -y git ansible python-pip python3-pip golang-go python-dnspython libpcap-dev libssl-dev libnetfilter-queue-dev
 pip install ldap3
 
 echo -e "\n\n"
 echo "[*] Installing tools from Kali repos..."
-sudo apt install oscanner tnscmd10g flameshot
-
-echo -e "\n\n"
-echo "[*] Setting ownership of /opt folder for GitHub repos..."
-sudo chown -R $username:$username /opt
+apt install oscanner tnscmd10g flameshot
 
 echo -e "\n\n"
 echo "[*] Cloning and installing the BrashEndeavours/hotwax repo..."
-cd ~
+cd /home/$username
 git clone https://github.com/BrashEndeavours/hotwax .hotwax
 cd .hotwax
 # Fixes issue with python-gnupg install in hotwax #
@@ -64,7 +60,7 @@ echo -e "\n\n"
 echo "[*] Cloning and setting up the maurosoria/dirsearch repo, and creating alias..."
 cd /opt
 git clone https://github.com/maurosoria/dirsearch.git
-echo "alias dirsearch='python3 /opt/dirsearch/dirsearch.py'" >> ~/.bash_aliases
+echo "alias dirsearch='python3 /opt/dirsearch/dirsearch.py'" >> /home/$username/.bash_aliases
 
 echo -e "\n\n"
 echo "[*] Cloning and setting up guelfoweb/knock repo..."
@@ -72,7 +68,7 @@ cd /opt
 git clone https://github.com/guelfoweb/knock.git
 cd knock
 python setup.py install
-#echo "alias knockpy='python /opt/knock/knockpy/knockpy.py'" >> ~/.bash_aliases
+#echo "alias knockpy='python /opt/knock/knockpy/knockpy.py'" >> /home/$username/.bash_aliases
 
 echo -e "\n\n"
 echo "[*] Cloning and setting alias for aboul3la/Sublist3r repo..."
@@ -80,7 +76,7 @@ cd /opt
 git clone https://github.com/aboul3la/Sublist3r.git sublister
 cd sublister
 pip install -r requirements.txt
-echo "alias sublister='python /opt/sublister/sublist3r.py'" >> ~/.bash_aliases
+echo "alias sublister='python /opt/sublister/sublist3r.py'" >> /home/$username/.bash_aliases
 
 echo -e "\n\n"
 echo "[*] Cloning and setting up vanhauser-thc/thc-ipv6 repo and setting aliases..."
@@ -96,16 +92,16 @@ cd /opt
 mkdir kerbrute
 cd kerbrute
 wget https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_linux_amd64
-echo "alias kerbrute='/opt/kerbrute/kerbrute_linux_amd64'" >> ~/.bash_aliases
+echo "alias kerbrute='/opt/kerbrute/kerbrute_linux_amd64'" >> /home/$username/.bash_aliases
 cd /opt
 git clone https://github.com/ropnop/windapsearch.git
-echo "alias windapsearch='python3 /opt/windapsearch/windapsearch.py'" >> ~/.bash_aliases
+echo "alias windapsearch='python3 /opt/windapsearch/windapsearch.py'" >> /home/$username/.bash_aliases
 
 echo -e "\n\n"
 echo "[*] Cloning 3ndG4me/AutoBlue-MS17-010 repo and setting nav alias..."
 cd /opt
 git clone https://github.com/3ndG4me/AutoBlue-MS17-010.git autoblue
-echo "alias goblue='cd /opt/autoblue'" >> ~/.bash_aliases
+echo "alias goblue='cd /opt/autoblue'" >> /home/$username/.bash_aliases
 
 echo -e "\n\n"
 echo "[*] Extracting rockyou and grabbing some more wordlists..."
@@ -117,11 +113,15 @@ unzip 1aNormusWL.zip
 rm 1aNormusWL.zip
 
 echo -e "\n\n"
+echo "[*] Setting ownership of /opt folder for GitHub repos..."
+chown -hR $username:$username /opt
+
+echo -e "\n\n"
 echo "[*] Setting up common aliases..."
-echo "alias updateme='apt update&&apt list --upgradeable'" >> ~/.bash_aliases
-echo "alias upgrademe='apt full-upgrade -y'" >> ~/.bash_aliases
-echo "alias sspt='searchsploit'" >> ~/.bash_aliases
-source ~/.bashrc
+echo "alias updateme='apt update&&apt list --upgradeable'" >> /home/$username/.bash_aliases
+echo "alias upgrademe='apt full-upgrade -y'" >> /home/$username/.bash_aliases
+echo "alias sspt='searchsploit'" >> /home/$username/.bash_aliases
+source /home/$username/.bashrc
 
 echo -e "\n\n"
 echo "[*] All done!"
