6958f8fec225f189e92749de33556328a58e5b78
David Martinez <dmartinez7500@gmail.com>
2020-01-29 13:49:41 -0500

    Added kate

--- setup_kali.sh
+++ setup_kali.sh
@@ -31,7 +31,7 @@ pip install ldap3
 
 echo -e "\n\n"
 echo "[*] Installing tools from Kali repos..."
-apt install oscanner tnscmd10g flameshot
+apt install oscanner tnscmd10g flameshot kate
 
 echo -e "\n\n"
 echo "[*] Cloning and installing the BrashEndeavours/hotwax repo..."
